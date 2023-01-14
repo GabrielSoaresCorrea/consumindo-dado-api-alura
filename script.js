2e424a289bd8b97701a158a5f120469e96531aac
unknown <gabrielsoares1602@gmail.com>
2023-01-14 02:39:28 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 async function buscaEndereco(cep) {
-    var mensagemErro = document.getElementById('erro');
-    mensagemErro.innerHTML = "";
+
+    const mensagemErro = document.getElementById('erro')
+    mensagemErro.innerHTML = ""
+
     try {
-        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-        var consultaCEPConvertida = await consultaCEP.json();
-        if (consultaCEPConvertida.erro) {
-            throw Error('CEP não existente!');
-        }
-        var cidade = document.getElementById('cidade');
-        var logradouro = document.getElementById('endereco');
-        var estado = document.getElementById('estado');
+      
+        const consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        const consultaCEPconvertida = await consultaCEP.json()
 
-        cidade.value = consultaCEPConvertida.localidade;
-        logradouro.value = consultaCEPConvertida.logradouro;
-        estado.value = consultaCEPConvertida.uf;
+        if(consultaCEPconvertida.erro) throw Error('CEP não existente!')
 
-        console.log(consultaCEPConvertida);
-        return consultaCEPConvertida;
-    } catch (erro) {
-        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
-        console.log(erro);
+        const cidade = document.getElementById('cidade')
+        const logradouro = document.getElementById('endereco')
+        const estado = document.getElementById('estado')
+
+        cidade.value = consultaCEPconvertida.localidade
+        logradouro.value = consultaCEPconvertida.logradouro
+        estado.value = consultaCEPconvertida.uf
+
+        console.log(consultaCEPconvertida)
+        return consultaCEPconvertida
+
+    } catch(erro){
+        mensagemErro.innerHTML = `<p>CEP não existente</p>`
+        console.log(erro)
+        
     }
 }
 
-var cep = document.getElementById('cep');
-cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+const cep = document.getElementById('cep')
+cep.addEventListener('focusout', () => {
+    buscaEndereco(cep.value)
+})
